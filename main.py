23d497960b0f043a91b4a119e054183d1a63cd75
Frell <marcin.tyszer@protonmail.com>
2024-01-24 23:28:42 +0100

    1

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import logging, sys
+import threading
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
 import argparse
 import pyfiglet
 
@@ -13,7 +16,7 @@ args = parser.parse_args()
 
 
 def print_logo():
-    logo = pyfiglet.figlet_format('|--SUMP--|', font='speed')
+    logo = pyfiglet.figlet_format('| SUMP |', font='speed')
     print(logo)
 
 
@@ -22,36 +25,50 @@ if args.url:
     url = args.url
     f = open(args.keywords, 'r')
     keywords = f.read().split(',')
+    f.close()
     output_file_name = args.output
 
     print_logo()
 
+    def refuse_cookies(drv):
+        drv.find_element(By.ID, 'onetrust-pc-btn-handler').click()
+        drv.find_element(By.CLASS_NAME, 'ot-pc-refuse-all-handler').click()
+
     driver = webdriver.Firefox()
     driver.get(url)
     driver.implicitly_wait(10)
+    refuse_cookies(driver)
 
 
-    def refuse_cookies():
-        driver.find_element(By.ID, 'onetrust-pc-btn-handler').click()
-        driver.find_element(By.CLASS_NAME, 'ot-pc-refuse-all-handler').click()
+    def search_for_keywords(auction_search_url, search_keywords):
+        driver.switch_to.new_window('tab')
+        driver.get(auction_search_url)
+        content = driver.find_elements(By.TAG_NAME, 'h1, h2, h3, h4, h5, h6, p')
+        for word in content:
+            if word.text in search_keywords:
+                return True
+            else:
+                return False
+        driver.close()
+        driver.switch_to.window(original_window)
 
-    refuse_cookies()
 
-    auction_list = driver.find_elements(By.CLASS_NAME, 'css-1sw7q4x')
+    original_window = driver.current_window_handle
+    assert len(driver.window_handles) == 1
+    auction_list = driver.find_elements(By.CSS_SELECTOR, 'div[data-testid="listing-grid"]')
     for item in auction_list:
         try:
-            auction = item.find_element(By.TAG_NAME, 'h6')
-            auction_title = auction.text
-            last_auction_title = ''
-            for keyword in keywords:
-                if keyword in auction_title:
-                    continue
-                if last_auction_title != auction_title:
-                    auction.click()
+            auctions = item.find_elements(By.TAG_NAME, 'a')
+            for auction in auctions:
+                auction_title = auction.find_element(By.TAG_NAME, 'h6').text
+                auction_url = auction.get_attribute('href')
+                if search_for_keywords(auction_url, keywords):
                     output_file = open(f'{output_file_name}', 'a')
-                    output_file.write(f'{auction_title}\n')
-                last_auction_title = auction_title
-
+                    output_file.write(f'{auction_title} : {auction_url}\n')
         except NoSuchElementException:
             pass
-    driver.close()
+    driver.quit()
+
+# TEST RUN
+#   python ./main.py -u "https://www.olx.pl/motoryzacja/q-miska-olejowa-bmw-e87/" -k input.txt -o output.txt
+#
