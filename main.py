107157b01e4f833dc24a9f54119471ce516b5daf
frell <marcin.tyszer@protonmail.com>
2024-01-27 13:11:40 +0100

    added: threading for searching for keywords function, timeout, also more exceptions.

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import threading
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import argparse
 import pyfiglet
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from selenium.webdriver.support.wait import WebDriverWait
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--url", help="provides site url")
-parser.add_argument("-k", "--keywords", help="keywords that you want to see, needs to be a txt file", type=argparse.FileType('r'))
+parser.add_argument("-k", "--keywords", help="keywords that you want to see, needs to be a txt file")
 parser.add_argument("-o", "--output", help="name a output txt file, where links from finished query will be provided")
 args = parser.parse_args()
 
@@ -35,7 +38,7 @@ class Sump:
         with open(self.keywords_file, 'r') as f:
             self.keywords = f.read().split(',')
 
-    def make_output(self, auction_title, auction_url):
+    def print_output(self, auction_title, auction_url):
         output_file = open(f'{self.output_filename}', 'a')
         output_file.write(f'{auction_title} : {auction_url}\n')
 
@@ -43,35 +46,56 @@ class Sump:
         self.driver.find_element(By.ID, 'onetrust-pc-btn-handler').click()
         self.driver.find_element(By.CLASS_NAME, 'ot-pc-refuse-all-handler').click()
 
-    def search_for_keywords(self, auction_search_urls, search_keywords):
-        for auction_search_url in auction_search_urls:
+    def search_for_keywords(self, auction_urls, timeout):
+        self.read_keywords()
+
+        def search_in_auction(url):
             self.driver.switch_to.new_window('tab')
-            self.driver.get(auction_search_url)
-            content = self.driver.find_elements(By.ID, 'text')
+            self.driver.get(url)
+            try:
+                content = WebDriverWait(self.driver, timeout).until(
+                    EC.presence_of_all_elements_located((By.ID, 'text'))
+                )
+            except TimeoutException:
+                self.driver.close()
+                self.driver.switch_to.window(self.original_window)
+                return False
+
+            found = False
             for word in content:
-                if word.text in search_keywords:
-                    return True
-                else:
-                    return False
+                if word.text in self.keywords:
+                    found = True
+                    break
             self.driver.close()
             self.driver.switch_to.window(self.original_window)
+            return found
+
+        with ThreadPoolExecutor() as executor:
+            futures = {executor.submit(search_in_auction, url): url for url in auction_urls}
+            for future in as_completed(futures):
+                url = futures[future]
+                try:
+                    result = future.result(timeout)
+                except TimeoutError:
+                    print(f"Timeout for url: {url}")
+                    continue
+                if result:
+                    return True
+            return False
 
     def get_auction_urls(self):
         auction_list = self.driver.find_elements(By.CSS_SELECTOR, 'div[data-testid="listing-grid"]')
+        auctions_urls = []
         for item in auction_list:
             try:
                 auctions = item.find_elements(By.TAG_NAME, 'a')
-                auctions_urls = []
                 for auction in auctions:
                     auction_title = auction.find_element(By.TAG_NAME, 'h6').text
                     auction_url = auction.get_attribute('href')
                     auctions_urls.append(auction_url)
-                if self.search_for_keywords(auctions_urls, self.keywords):
-                    self.make_output(auction_title, auction_url)
-                else:
-                    pass
             except NoSuchElementException:
                 pass
+        return auctions_urls
 
     def run(self):
         print_logo()
@@ -81,7 +105,9 @@ class Sump:
         self.refuse_cookies()
         self.original_window = self.driver.current_window_handle
         assert len(self.driver.window_handles) == 1
-        self.get_auction_urls()
+        auction_urls = self.get_auction_urls()
+        self.read_keywords()
+        self.search_for_keywords(auction_urls, 10)
         self.driver.quit()
 
 
